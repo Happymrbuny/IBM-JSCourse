a16831cc58500b820fe3d5d5b2f6adf8d9414b90
Aaron Phelps <happymrbunny@gmail.com>
2024-03-13 17:31:44 -0400

    update controlFlow

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -1,18 +1,5 @@
-let userType = "subscriber";
-let userCategory;
+let isAuthenticated = true;
 
-switch (userType) {
-    case "admin":
-        userCategory = "Administrator";
-        break;
-    case "manager":
-        userCategory = "Manager";
-        break;
-    case "subscriber":
-        userCategory = "Subscriber";
-        break;
-    default:
-        userCategory = "Unknown";
-}
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
-console.log("User Category:", userCategory);
+console.log("Authentication Status:", authenticationStatus);
