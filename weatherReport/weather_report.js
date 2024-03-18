f83c8315fea17042647c7866ad134f642c009f2b
Aaron Phelps <59273003+Happymrbuny@users.noreply.github.com>
2024-03-18 16:32:32 -0500

    Update weather_report.js

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -2,7 +2,7 @@ function showweatherDetails(event){
     event.preventDefault();
 
     const city = document.getElementById('city').value;
-    const apiKey = 'be7617f1b57d14ba841f32c9f03fad08'
+    const apiKey = xxx
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
